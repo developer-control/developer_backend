643640d3529b37db6442e49df7973657619da7d0
akmal subakti <akmalsubakti@akmals-MacBook-Pro.local>
2025-01-04 12:13:34 +0700

    update get banner query request permission

--- app/Http/Requests/Api/BannerQuery.php
+++ app/Http/Requests/Api/BannerQuery.php
@@ -11,7 +11,7 @@ class BannerQuery extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class BannerQuery extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "limit" => 'int',
+            "search" => 'string',
+            "developer_id" => 'int',
+            /**
+             * Page number
+             * 
+             * @example 1
+             */
+            'page' => 'int',
         ];
     }
 }
