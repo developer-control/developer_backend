4278bd8a483b8c89ed998c309763d3031ba23c83
akmal subakti <akmalsubakti@akmals-MacBook-Pro.local>
2025-01-13 20:37:43 +0700

    update seeder database for remove directory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +20,6 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProjectSeeder::class,
         ]);
+        Storage::deleteDirectory(storage_path('app/public'));
     }
 }
